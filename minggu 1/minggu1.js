d93b16b2adda6e026adb78ba3212d67d060207b0
qiuu <konobaka1655@gmail.com>
2023-10-18 12:30:32 +0700

    revisi

--- minggu 1/minggu1.js
+++ minggu 1/minggu1.js
@@ -5,12 +5,10 @@
 
 function bandingkanAngka(angka1, angka2) {
   let hasil = ""
-if(angka1 < angka2){hasil = true;}
-else if (angka1 > angka2){hasil = false;}
-else if (angka1 = angka2){hasil = -1;}
-else{"input yang anda masukan salah"}
+if(angka1 < angka2){return true;}
+else if (angka1 > angka2){return false;}
+else{return -1}
 
-return hasil;
   }
     
     // TEST CASES
