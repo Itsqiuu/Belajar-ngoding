c969451d212bdc15ad0d975d549a2beee9900acb
qiuu <konobaka1655@gmail.com>
2023-10-18 12:17:12 +0700

    ujian minggu1

---
+++ minggu 1/minggu1_2.js
@@ -0,0 +1,59 @@
+// Problem
+// Diberikan sebuah function konversiMenit(menit) yang menerima satu parameter berupa angka yang merupakan ukuran waktu 
+// dalam menit. Function akan me-return string waktu dalam format jam:menit berdasarkan menit tersebut. 
+// Contoh, jika menit adalah 63, maka function akan me-return "1:03".
+
+function konversiMenit(menit) {
+    jam = Math.floor(menit/60)
+    sisa = menit % 60
+    men = ""
+    if (sisa < 10){
+        men = "0"+sisa
+    }
+    else{
+        men = sisa
+    }
+    return jam +":" +men
+    
+}
+  
+  // TEST CASES
+  console.log(konversiMenit(63)); // 1:03
+  console.log(konversiMenit(124)); // 2:04
+  console.log(konversiMenit(53)); // 0:53
+  console.log(konversiMenit(88)); // 1:28
+  console.log(konversiMenit(120)); // 2:00
+
+// ------------------------------------
+console.log('')
+// ------------------------------------
+
+//   Problem
+//   Diberikan sebuah function xo(str) yang menerima satu parameter berupa string. 
+//   Function akan me-return true jika jumlah karakter x sama dengan jumlah karakter o, dan false jika tidak.
+
+function xo(str) {
+    x = ""
+    o = ""
+ for (let i = 0;i < str.length;i++){
+    if (str[i] === "x"){
+        x++;
+    }
+    else {
+        o++;
+    }
+ }
+ if( x == o ){
+    return true
+ }
+ else{
+    return false
+ }
+}
+  
+  // TEST CASES
+  console.log(xo('xoxoxo')); // true
+  console.log(xo('oxooxo')); // false
+  console.log(xo('oxo')); // false
+  console.log(xo('xxxooo')); // true
+  console.log(xo('xoxooxxo')); // true
